49c7015e58243aab1ad0e15b295a9d33631a466c
jgomez2531 <76822966+jgomez2531@users.noreply.github.com>
2021-06-20 15:01:52 +0200

    CSS

--- src/components/Search/search.js
+++ src/components/Search/search.js
@@ -2,13 +2,17 @@ import React from 'react';
 import './search.css';
 
 const Search = props => {
-    return (<div className="input-group searchInput">
+    return (<div className="divSearch">
+    <h2>Bienvenidos.</h2>
+    <h3>Millones de películas, programas de televisión y personas por descubrir. Explora ahora.</h3>
+    <div className="input-group searchInput">
     <input type="text" className="form-control inputSearch" placeholder="Buscar una película..."/>
     <div className="input-group-append">
       <button className="btn btn-search" type="button">
         <i className="fa fa-search"></i>
       </button>
     </div>
+  </div>
   </div>)
 };
 
